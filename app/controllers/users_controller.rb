66f35960a0af263465002801f2aceea793ff15ca
Okamotom <ec2-user@ip-172-31-2-152.us-east-2.compute.internal>
2018-03-10 07:52:21 +0000

    favor, unfavor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers , :favorposts]
 
   def index
     @users = User.all.page(params[:page])
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @micropost = Micropost.find(params[:id])
     @microposts = @user.microposts.order('created_at DESC').page(params[:page])
     counts(@user)
   end
@@ -36,6 +37,12 @@ class UsersController < ApplicationController
   def followers
     @user = User.find(params[:id])
     @followers = @user.followers.page(params[:page])
+    counts(@user)
+  end
+  
+  def favorposts
+    @user = User.find(params[:id])
+    @favorposts = @user.favorposts.page(params[:page])
     counts(@user)
   end
 
